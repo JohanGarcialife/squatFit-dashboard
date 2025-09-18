1e2b4db7bb9c9dacf9bfb9a6c84218cf42f671e8
Johan <johan@Johans-MacBook-Air.local>
2025-09-18 18:04:29 -0400

    Chat anadido

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -4,7 +4,7 @@ export const APP_CONFIG = {
   name: "Squat Fit Back Office",
   version: packageJson.version,
   meta: {
-    title: "Squat Fit Back Office",
+    title: "Squat Fit Back Office - with chat",
     description:
       "Squat Fit Back Office es un dashboard moderno construido con Next.js 15, Tailwind CSS v4 y shadcn/ui.",
   },
